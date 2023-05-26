a3c4f8314b23524c0713afec9fc31092a6849ad4
acecic82 <102938al@naver.com>
2023-05-27 03:33:45 +0900

    grade파일 commit

---
+++ src/test/java/com/example/managingmember/ManagingmemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auctiontoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionToyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
